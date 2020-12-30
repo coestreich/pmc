2809a4be328439e627c89318f14b248523657ed8
coestreich <coestrei@mit.edu>
2020-12-30 15:07:49 -0500

    Fixed driver error.

--- pmc_driver.cpp
+++ pmc_driver.cpp
@@ -110,7 +110,7 @@ int main(int argc, char *argv[]) {
         //cout << "Size (omega): " << C.size() << endl;
         print_max_clique(C);
 
-        if (C.size() < in.param_ub)
+        //if (C.size() < in.param_ub)
           //  cout << "Clique of size " << in.param_ub << " does not exist." <<endl;
     }
     C.clear();
